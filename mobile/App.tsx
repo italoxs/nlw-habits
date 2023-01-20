37923fb58544fc40e6a590890a05a3f3b0688ae7
Italo Sales <italo.x.sales@hotmail.com>
2023-01-20 11:59:04 -0300

    day3 - back, front and mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,36 @@
+import './src/lib/dayjs'
+
+import { StatusBar } from 'expo-status-bar';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter'
+
+import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
+  return (
+    <>
+      <StatusBar style='light' backgroundColor='transparent' translucent />
+      <Routes />
+    </>
+  );
+}
---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
+
+const app = Fastify();
+
+app.register(cors, {
+  origin: ["http://localhost:3000"],
+});
+app.register(appRoutes);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running");
+  });
